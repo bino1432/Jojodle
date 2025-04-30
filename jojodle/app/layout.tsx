c3a11a6ae73aaada00d64466a01fb609760cfae9
Bernardo P Zapelini <bernardozplz@gmail.com>
2025-04-30 19:11:42 -0300

    👍

--- jojodle/app/layout.tsx
+++ jojodle/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Header from "@/components/Header";
 import Home from "./page";
 import "./globals.css";
 
@@ -16,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[url(@/public/images/image/background-image.png)] bg-repeat">
-        <Header/>
         {children}
       </body>
     </html>
--- jojodle/app/page.tsx
+++ jojodle/app/page.tsx
@@ -1,8 +1,12 @@
+import Header from "@/components/Header";
 import MenuButton from "@/components/MenuButton";
 import starIcon from "@/public/images/icon/Star-Icon.png"
 
 export default function Home() {
   return (
-    <MenuButton img={starIcon} title={"Classic"} alt={"Classic Icon"}></MenuButton>
+    <main>
+      <Header/>
+      <MenuButton img={starIcon} title={"Classic"} alt={"Classic Icon"}></MenuButton>
+    </main>
   );
 }
