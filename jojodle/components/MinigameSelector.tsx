9939581cff71c7396a81503b6f78ff1e5112bc82
bino1432 <bernardozplz@gmail.com>
2025-05-10 01:20:43 -0300

    NADA FUNCIONA

--- jojodle/components/MinigameSelector.tsx
+++ jojodle/components/MinigameSelector.tsx
@@ -5,12 +5,15 @@ import ClassicIcon from "@/public/images/icon/Star-Icon.svg"
 import StandIcon from "@/public/images/icon/Stand-Icon.svg"
 import QuoteIcon from "@/public/images/icon/Quote-Icon.svg"
 import PoseIcon from "@/public/images/icon/Pose-Icon.svg"
+import { usePathname } from "next/navigation"
 
 export default function MinigameSelector() {
+    const pathname = usePathname();
+
     return(
         <div className="flex gap-2">
             <Link className="" href={"/Classic"}>
-                <Image className={`${window.location.pathname === '/' ? 'text-white' : 'text-white'}`} src={ClassicIcon} alt="Classic Icon" />
+                <Image className={`${pathname === '/Classic' ? 'text-blue-500' : 'text-blue-500' }`} src={ClassicIcon} alt="Classic Icon" />
             </Link>
             <Link href={"/Stand"}>
                 <Image src={StandIcon} alt="Stand Icon" />
