bfbddae4278877cf58cf7379922839c65f788820
Bernardo P Zapelini <bernardozplz@gmail.com>
2026-05-22 01:18:05 -0300

    😂

--- jojodle/app/Stand/page.tsx
+++ jojodle/app/Stand/page.tsx
@@ -13,8 +13,8 @@ import SearchInput from "@/components/UniversalComponents/SearchInput";
 import StandCard from "@/components/StandComponents/StandCard";
 
 const archivoBold = Archivo({
-  subsets: ['latin'],
-  weight: "700",
+    subsets: ['latin'],
+    weight: "700",
 },);
 
 interface StandInfo {
@@ -34,12 +34,12 @@ interface Character {
 export default function Standpage() {
 
     useEffect(() => {
-    const randomCharacter = standJson[Math.floor(Math.random() * standJson.length)]
-    setCorrectCharacter(randomCharacter);
-    setShowedStand(verifyStand(randomCharacter.Stands));
-    console.log(attempts)
+        const randomCharacter = standJson[Math.floor(Math.random() * standJson.length)]
+        setCorrectCharacter(randomCharacter);
+        setShowedStand(verifyStand(randomCharacter.Stands));
+        console.log(attempts)
     }, []);
-    
+
     const [correctCharacter, setCorrectCharacter] = useState<Character | null>(null);
 
     useEffect(() => {
@@ -51,22 +51,33 @@ export default function Standpage() {
     const [winGame, setWinGame] = useState(false);
     const [correctStandIndex, setCorrectStandIndex] = useState(0)
     const [showedStand, setShowedStand] = useState("");
+    const [currentHint, setCurrentHint] = useState("");
+    const [side, setSide] = useState("");
+    const [isLeft, setIsLeft] = useState(false);
+    const [isRight, setIsRight] = useState(false);
 
     const reciveCharacterIdFromComponent = (id: number) => {
-    setTriedCharacter([...triedCharacter, id]);
-      if (triedCharacter.length !== 0) {
-        setAttempts(attempts + 1);
-      }
-      console.log(attempts);
-      console.log(triedCharacter);
+        setTriedCharacter([...triedCharacter, id]);
+        if (triedCharacter.length !== 0) {
+            setAttempts(attempts + 1);
+        }
+        console.log(attempts);
+        console.log(triedCharacter);
     }
 
     const reciveIfWinGame = (win: boolean) => {
         setWinGame(win);
     }
 
+    const reciveHintAndSideFromComponent = (hint: string, side: string, left: boolean, right: boolean) => {
+        setCurrentHint(hint);
+        setSide(side);
+        setIsLeft(left);
+        setIsRight(right);
+    };
+
     const verifyStand = (stand: StandInfo[]) => {
-        if(stand.length === 1){
+        if (stand.length === 1) {
             console.log(stand[0])
             return stand[0].Stand
         } else {
@@ -83,9 +94,19 @@ export default function Standpage() {
             <div>
                 <Header />
                 <MinigameSelector />
-                <div className="flex flex-col p-4 bg-[var(--Background)] items-center text-center mt-4 max-w-110 rounded-lg m-auto gap-4">
-                    <p className={`${archivoBold.className} text-xl text-white leading-5.5 text-balance`}>Take a guess at today's JoJo's Bizarre Adventure Stand user!</p>
+                <div className="flex flex-col p-4 bg-[var(--Background)] items-center text-center mt-4  size-fit rounded-lg m-auto gap-4">
+                    <p className={`${archivoBold.className} text-xl text-white leading-5.5 text-balance`}>Take a guess at today's JoJo's <br/> Bizarre Adventure Stand user!</p>
                     <p className={`${archivoBold.className} text-2xl text-white leading-6.5`}>「{showedStand}」</p>
+                    <div className="flex gap-4">
+                        {triedCharacter.length !== 0 && correctCharacter ? (<>
+                            <HintButtons title="Type Clue" guesses={3} image={TypeClueIcon} attempts={attempts} hint={correctCharacter.Stands[correctStandIndex]["Stand Type"]} reciveHintAndSide={reciveHintAndSideFromComponent} side={"left"} isRounded={isRight} />
+                            <HintButtons title="Part Clue" guesses={5} image={PartClueIcon} attempts={attempts} hint={correctCharacter.Stands[correctStandIndex].Debut} reciveHintAndSide={reciveHintAndSideFromComponent} side={"middle"} isRounded={isLeft} />
+                            <HintButtons title="Technique Clue" guesses={7} image={TechniqueClueIcon} attempts={attempts} hint={correctCharacter.Stands[correctStandIndex].Technique} reciveHintAndSide={reciveHintAndSideFromComponent} side={"right"} isRounded={isLeft} />
+                        </>) : null
+                        }
+                    </div>
+                    <p className={currentHint == "" ? "hidden" : `${archivoBold.className} text-white p-2 w-137 bg-[var(--Primary)] rounded-lg
+                    ${side == "left" ? "rounded-tl-none" : "rounded-tr-none"}`} id="fadeIn">{currentHint}</p>
                 </div>
 
                 <div>
@@ -97,7 +118,7 @@ export default function Standpage() {
                         <div className="flex flex-col-reverse relative">
                             {triedCharacter.map((id) => {
                                 const characterData = standJson.find(char => char.ID === id);
-                                if(!characterData) return null
+                                if (!characterData) return null
 
                                 return (
                                     correctCharacter && (
