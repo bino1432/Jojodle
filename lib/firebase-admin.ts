7ed1afb0bd2149a3497cd2c63344675f2ca4fb6a
Guilherme Schneider <princecronoszeus@gmail.com>
2026-06-02 14:46:31 -0300

    env variable name fix

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase-admin/firestore";
 function getAdminApp() {
   if (getApps().length > 0) return getApps()[0];
 
-  const projectId = process.env.FIREBASE_ADMIN_PROJECT_ID;
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
   const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
