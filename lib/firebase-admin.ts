9d8248f9c93ad646472808a36f00a05d5b91f300
Guilherme Schneider <princecronoszeus@gmail.com>
2026-06-02 14:51:28 -0300

    base64 private key

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,9 +4,14 @@ import { getFirestore } from "firebase-admin/firestore";
 function getAdminApp() {
   if (getApps().length > 0) return getApps()[0];
 
+  
+
   const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n");
+  const privateKey = Buffer.from(
+    process.env.FIREBASE_ADMIN_PRIVATE_KEY!,
+    "base64"
+  ).toString("utf-8");
 
   if (!projectId || !clientEmail || !privateKey) {
     throw new Error("Missing Firebase Admin env vars");
