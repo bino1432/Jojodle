5ed46fa4ced21e88e251a87c3ea249fb19d1cd28
Bernardo P Zapelini <bernardozplz@gmail.com>
2026-09-24 14:34:32 -0300

    a

--- lib/firebase-admin.tsx
+++ lib/firebase-admin.tsx
@@ -7,9 +7,9 @@ function getFirebaseAdminApp(): App {
 
   return initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_ADMIN_CLIENT_EMAIL?.replace(/\\n/g, "\n"),
     }),
   });
 }
